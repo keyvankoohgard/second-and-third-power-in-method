8797546603a72134453de1c01109aedd6ff2f9b1
keyvan <keyvankoohgard61@gmail.com>
2022-11-19 16:46:40 +0330

    Add project files.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,20 @@
+﻿// getting second and third power of a number in metho  \\
+using System.Runtime.CompilerServices;
+
+Console.WriteLine("please enter a number:");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(calc(x));
+
+
+double calc(int x)
+{
+    double result = Math.Pow(x, 2);
+    return result;
+}
+Console.WriteLine(calcg(x));
+double calcg(int x)
+{
+     double result_2 = Math.Pow(x, 3);
+    return result_2;
+}
